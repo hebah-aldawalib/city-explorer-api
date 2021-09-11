0bdfceb7bb080a2d5e8fa2009443054d91795ceb
hebah al daealieb <hebah20082gmail.com>
2021-09-11 23:45:09 +0300

    edit

---
+++ helper/cach.helper.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+class Cache {
+
+
+  constructor()    {
+
+   
+ this.forCast = [];
+
+    this.movies = [];
+
+    // this.weather=[];
+
+    this.timeStamp = Date.now();
+
+
+  }
+}
+
+    module.exports = Cache;
