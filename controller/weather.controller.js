337597f6d5697e64f247685f7eb623692174742c
hebah al daealieb <hebah20082gmail.com>
2021-09-14 22:58:44 +0300

    server Edit

--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -3,25 +3,31 @@ const axios = require("axios");
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const Forecast =require('../models/weather.model');
 require('dotenv').config();
-const getweathers= async (request, res) => {
+
+const getWeather = async (request, res) => {
     const city = request.query.city;
+    try{
     const weatherUrl = `https://api.weatherbit.io/v2.0/forecast/daily?&city=${city}&key=${WEATHER_API_KEY}`
     //  lat=${lat}&key=${WEATHER_API_KEY}&lon=${lon}
 
-    const weatherBitResponse = await axios.get(`${weatherUrl}`);
+    
     //  response.json(weatherBitResponse.data);
 
     if (city) {
+        const weatherBitResponse = await axios.get(weatherUrl);
         let newArray = weatherBitResponse.data.data.map(item => {
             return new Forecast(item.datetime, item.weather.description)
-        })
-        if (newArray.length)
-            console.log(newArray);
+        });
+        // if (newArray.length)
+        //     console.log(newArray);
         res.json(newArray)
     } else {
-        res.json('no data ')
+        res.send('no data ')
+    }} catch(err) {
+        console.log("Something went wrong!");
+
     }
-}
+ }
 // const getHello =
 //     function (req, res) { // callback function of what we should do with our request
 //         res.send('nice') // our endpoint function response
--- server.js
+++ server.js
@@ -1,35 +1,23 @@
-const express = require('express') // require the express package
-const app = express() // initialize your express app instance
+const express = require("express"); // require the express package
+const app = express(); // initialize your express app instance
+require('dotenv').config();
+const cors = require("cors");
+const getWeather = require("./controller/weather.controller");
+const getMovies = require("./controller/movi.controller");
+app.use(cors());
 
-const cors = require('cors');
-const getWeather = require('./controller/weather.controller');
-const getMovies= require('./controller/movi.controller');
-app.use(cors())
+const PORT = process.env.PORT;
+const weather = require("./data/weather.json");
+const { response } = require("express");
+const { getIndex, getHello } = require("./controller/index.controller");
 
+app.get("/", getIndex);
+app.get("/ getHello", getHello);
 
+app.get("/weather", getWeather);
 
-const PORT = process.env.PORT
-const weather = require('./data/weather.json');
-const { response } = require('express');
-const {
+app.get("/movies", getMovies);
 
-    getIndex,
-    getHello
-  } = require('./controller/index.controller');
-  
-
-
-
-
-
-    app.get('/', getIndex)
-    app.get('/ getHello',  getHello)
-
-app.get('/weather', getWeather);
-
-
-
-app.get('/movies', getMovies);
-
-
-app.listen(PORT)
+app.listen(PORT,()=>{
+  console.log("Server is running on port 3030.");
+});
